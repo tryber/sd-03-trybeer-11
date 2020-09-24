ff2a2bbb4c3e07664f8fc462d97445e2723b126f
Jafet Henrique <jafetguerra6@gmail.com>
2020-09-24 16:06:09 -0300

    Pagina CLiente-produtos-pronta

--- front-end/src/Pages/Client/Client-Product/index.jsx
+++ front-end/src/Pages/Client/Client-Product/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getApiData } from '../../../Redux/action/apiProductsAction';
 import ProductCard from './innerPage/ProductCard';
@@ -16,19 +17,33 @@ const ClientProduct = () => {
   const totalPriceBRL = totalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}); 
   // https://pt.stackoverflow.com/questions/181922/formatar-moeda-brasileira-em-javascript/186798
 
+  const goCart = () => {
+    localStorage.setItem('sellingProducts', JSON.stringify(shoppingList));
+  };
+
   useEffect(() => {
     dispatch(getApiData());
   }, []);
 
   return (
-    <div>
-      <div className="cards-container">
-        {products.map(({ id, name, price, urlImage }, index) => (
-          <ProductCard key={id} id={id} photo={urlImage} name={name} price={price} index={index} />
-        ))}
+    <div className="general-container">
+      <div>
+        <div className="cards-container">
+          {products.map(({ id, name, price, urlImage }, index) => (
+            <ProductCard key={id} id={id} photo={urlImage} name={name} price={price} index={index} />
+          ))}
+        </div>
+        <Link to="/checkout">
+          <button
+            className="checkout-button"
+            onClick={goCart}
+            data-testid="checkout-bottom-btn"
+          >
+            Ver Carrinho
+          </button>
+        </Link>
       </div>
-      <button data-testid="checkout-bottom-btn">Ver Carrinho</button>
-      <span data-testid="checkout-bottom-btn-value">{totalPriceBRL}</span>
+      <span data-testid="checkout-bottom-btn-value">{`Total: ${totalPriceBRL}`}</span>
     </div>
   )
 }
