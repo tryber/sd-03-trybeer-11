7a892273d0f88ff2c16d6ac3f8e07e21496fd598
Gustaff <gustavohmfigueiredo@gmail.com>
2020-09-24 16:11:47 -0300

    register name e email errors

--- back-end/tests/register.test.js
+++ back-end/tests/register.test.js
@@ -21,6 +21,11 @@ const config = {
 const URL_BASE = `http://localhost:${PORT}`;
 
 describe('register', () => {
+  const nameError = 'pelo menos 12 caracteres, não pode conter numeros nem caracteres especiais';
+  const emailError = 'email tem que ser no formato <nome@dominio>';
+  const passwordError = 'senha de pelo menos 6 digitos';
+  const lessInfoError = 'Faltando informacoes';
+  const emailDuplicatedError = 'email ja existe';
   let conn;
   let db;
 
@@ -49,13 +54,74 @@ describe('register', () => {
     };
 
     await frisby.post(`${URL_BASE}/user`, user)
-      .inspectJSON()
       .expect('status', 201)
       .expect('jsonTypes', {
         id: Joi.number().required(),
         email: 'example@example.com',
+        token: Joi.string().required(),
         name: 'exampleGrande',
         role: 'client',
       });
+  });
+
+  test('Name should have at least 12 characters', async () => {
+    const user = {
+      name: 'abcdefghijk',
+      email: 'example@example.com',
+      password: 'abcdef',
+      role: 'false',
+    };
+
+    await frisby.post(`${URL_BASE}/user`, user)
+      .expect('status', 422)
+      .expect('jsonTypes', {
+        message: nameError,
+      });
+  });
+
+  test('Name should not have number', async () => {
+    const user = {
+      name: 'abcdefghijk123',
+      email: 'example@example.com',
+      password: 'abcdef',
+      role: 'false',
+    };
+
+    await frisby.post(`${URL_BASE}/user`, user)
+      .expect('status', 422)
+      .expect('jsonTypes', {
+        message: nameError,
+      });
+  });
+
+  test('Name should not have special characters', async () => {
+    const user = {
+      name: 'abcdefghijk@^',
+      email: 'example@example.com',
+      password: 'abcdef',
+      role: 'false',
+    };
+
+    await frisby.post(`${URL_BASE}/user`, user)
+      .expect('status', 422)
+      .expect('json', {
+        message: nameError,
+      });
+  });
+
+  test('email should have the <name>@<dominio> format', (done) => {
+    const user = {
+      name: 'abcdefghijkl',
+      email: 'example@',
+      password: 'abcdef',
+      role: 'false',
+    };
+
+    frisby.post(`${URL_BASE}/user`, user)
+      .expect('status', 422)
+      .expect('json', {
+        message: emailError,
+      })
+      .then(() => done());
   });
 });
