5e74de57759f7448fc29badcbd5d90127637ce63
Gustaff <gustavohmfigueiredo@gmail.com>
2020-09-28 19:26:36 -0300

    lint

--- back-end/controllers/sales.js
+++ back-end/controllers/sales.js
@@ -2,9 +2,7 @@ const rescue = require('express-rescue');
 const { salesServices } = require('../services');
 
 const getAllSales = rescue(async (_req, res, _next) => {
-  console.log('chegou aqui');
   const sales = await salesServices.getAll();
-  console.log('sales');
   res.status(200).json({ sales });
 });
 
