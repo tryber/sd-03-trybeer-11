68bc9d0ab5454bb1360e2b0eabd30e2b859298e5
Jafet Henrique <jafetguerra6@gmail.com>
2020-09-29 17:26:52 -0300

    datatesid qnt colocado

--- front-end/src/Pages/Client/Checkout/innerPage/CheckoutProductCard.jsx
+++ front-end/src/Pages/Client/Checkout/innerPage/CheckoutProductCard.jsx
@@ -4,7 +4,7 @@ import convertBRL from '../../../../Services/BRLFunction'
 const CheckoutProductCard = ({ index, id, name, price, sellingQnt, updateLocalStorage }) => {
   return (
     <div>
-      <span>{index + 1}</span>
+      <span data-testid={`${index}-product-qtd-input`}>{sellingQnt}</span>
       <h4 data-testid={`${index}-product-name`}>{name}</h4>
       <span data-testid={`${index}-product-total-value`}>{convertBRL(price * sellingQnt)}</span>
       <span data-testid={`${index}-product-unit-price`}>{`${convertBRL(price)} un`}</span>
